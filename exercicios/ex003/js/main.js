05481d6fde0117d8a6afe81ebfe2bf627b8b9f45
MakaulydaSilva <makauly123@outlook.com>
2022-07-05 22:55:33 -0300

    att

---
+++ exercicios/ex003/js/main.js
@@ -0,0 +1,11 @@
+
+/*
+var fruta = {nome:"maça", cor: "vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+var frutas = [{nome:"maça", cor: "vermelha"}, {nome:"uva", cor: "roxa"}];
+
+console.log(frutas);
+alert(frutas[1].nome);
