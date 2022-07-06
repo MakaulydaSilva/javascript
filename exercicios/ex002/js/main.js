38947863914492b39675cb47d2b7613be49b76f0
MakaulydaSilva <makauly123@outlook.com>
2022-07-05 22:37:44 -0300

    att

---
+++ exercicios/ex002/js/main.js
@@ -0,0 +1,14 @@
+var lista = ["maça", "pêra", "laranja"];
+//lista.pop(); //pop= retira elemento da variavel
+lista.push("uva"); //push = adicionar elemento a variavel
+ 
+console.log(lista);
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+alert(lista[1]);
+console.log(lista.length); //mostra a quantidade de elementos na lista
+
+console.log(lista.reverse()); //reverte a lista
+console.log(lista[0])
+console.log(lista.toString()[0]); //transforma a lista em string
+console.log(lista.join(" - "));
