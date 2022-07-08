77159f1f8b776c7983168c382d4def1570e6bbca
MakaulydaSilva <makauly123@outlook.com>
2022-07-07 23:47:12 -0300

    att

--- ex005/js/main.js
+++ ex005/js/main.js
@@ -1,9 +1,53 @@
 
+
+function clicou(){
+   
+     document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+     //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://www.youtube.com/");
+   // window.location.href = "https://www.youtube.com/";
+}
+
+function trocar (elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto")
+    elemento.innerHTML = "Obrigado por passar o mouse";
+
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+
+
+}
+
+function load(){
+    alert("pagina carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+
+
+
+
+
+
+
+
+
+/*
 function soma(n1, n2){
     return n1 + n2;
 } 
 
-
+ 
 var validar = 0;
 
 function validaIdade(idade){
@@ -21,4 +65,6 @@ function validaIdade(idade){
 var idade = prompt("Qual sua idade?");
 console.log(validaIdade(idade));
 
-alert(soma(5, 10));
+alert(soma(5, 10)); 
+
+*/
