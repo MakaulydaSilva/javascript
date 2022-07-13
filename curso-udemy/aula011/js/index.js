d4bf2b9f5c13322bbf6cc33067d11b783bfe32b1
MakaulydaSilva <makauly123@outlook.com>
2022-07-13 17:55:52 -0300

    aula 10

---
+++ curso-udemy/aula011/js/index.js
@@ -0,0 +1,11 @@
+const num1 = prompt('Digite um número');
+const num2 = prompt('Digite outro número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+console.log(typeof num1);
+console.log(typeof num2);
+
+
+
