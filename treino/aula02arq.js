b58fc68b06ca8ceba1744062322cf82ffbbef5cd
MakaulydaSilva <makauly123@outlook.com>
2022-07-13 13:29:20 -0300

    exercicio

---
+++ treino/aula02arq.js
@@ -0,0 +1,25 @@
+
+//Trabalho da aula 2 de arquitetura computacional
+
+const livros = 1000;
+const paginas = 150;
+const palavrasDeUmaPagina = 200;
+const caracteresEmCadaPalvra = 7;
+
+console.log(livros * paginas);
+
+const totalPaginas = 150000;
+
+console.log(totalPaginas * palavrasDeUmaPagina);
+
+const totalDePalavras = 30000000;
+
+console.log(totalDePalavras * caracteresEmCadaPalvra);
+
+const totalDeCaracteres = 210000000
+const byte = 8; // 1byte = 8Bits
+
+console.log(totalDeCaracteres * byte);
+
+const totalEmBits = 1680000000; // 1680000000Bits ou 200.27Mb
+
