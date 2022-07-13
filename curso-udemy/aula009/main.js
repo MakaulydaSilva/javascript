6b2a3d54d9deb4b09aa17c9ab32afcb65c04c092
MakaulydaSilva <makauly123@outlook.com>
2022-07-12 22:25:45 -0300

    aula 09

---
+++ curso-udemy/aula009/main.js
@@ -0,0 +1,27 @@
+const nome = 'Luiz'; // String
+const nome1 = "Luiz"; // String
+const nome2 = `Luiz`; // String
+const num1 =  10; // Number
+const num2 = 10.52 // Number
+let nomeAluno; //undefined -> não aponta pra local nenhum da memória
+const sobrenomeAluno = null; // Nulo -> não aponta pra local nenhum da memória
+const aprovado = false; //Boolean = true, false (lógico)
+
+console.log(typeof num1, num1);
+console.log(typeof nomeAluno, nomeAluno);
+console.log(typeof nome1, nome1);
+console.log(typeof sobrenomeAluno, sobrenomeAluno);
+console.log(typeof aprovado, aprovado);
+
+//TIPOS DE DADOS PRIMITIVOS: string, Number, Undefined, Null, Boolean
+let a = 2;
+const b = a;
+console.log(a, b);
+
+
+a = 3;
+console.log(a, b);
+
+
+
+
