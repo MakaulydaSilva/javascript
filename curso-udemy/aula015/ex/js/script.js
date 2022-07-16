88e8080c917e73a061bf37d89d1944a00e128355
MakaulydaSilva <makauly123@outlook.com>
2022-07-15 23:27:13 -0300

    aula 15

--- curso-udemy/aula015/ex/js/script.js
+++ curso-udemy/aula015/ex/js/script.js
@@ -1,5 +1,17 @@
-const numero = prompt('Digite um número:');
+const numero = Number(prompt('Digite um número:'));
 const num1 = document.getElementById('num1');
 const texto = document.getElementById('texto');
 
 num1.innerHTML = numero;
+
+texto.innerHTML += `<p>Raiz  quadrada: ${numero ** 0.5}.<p/> `;
+
+texto.innerHTML += `<p> ${numero} é inteiro: ${Number.isInteger(numero)} .<p/> `;
+
+texto.innerHTML += `<p>É NaN: ${Number.isNaN(numero)}.<p/>`;
+
+texto.innerHTML += `<p>Arredondo para baixo: ${Math.floor(numero)}.<p/>`;
+
+texto.innerHTML += `<p>Arredondo para cima: ${Math.ceil(numero)}.<p/>`;
+
+texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}.<p/>`;
