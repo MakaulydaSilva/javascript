b08c8c5240df59927758acb338aafa1b061704fe
Makauly Silva <makauly123@outlook.com>
2022-08-07 18:57:51 -0300

    criado o arquivojs do projeto

---
+++ curso-udemy/ex-05-calculadora-function-factory/assets/js/calculadora.js
@@ -0,0 +1,14 @@
+function criaCalculadora() {
+     return {
+        ativaDisplay: document.querySelector('display'),
+
+        btnClear: document.querySelector('btn-clear'),
+        
+        inicia() {
+
+        } 
+     }
+}
+
+const calculadora = criaCalculadora;
+calculadora.inicio
