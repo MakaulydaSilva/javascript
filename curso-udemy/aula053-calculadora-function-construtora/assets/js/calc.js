7b59aa6f25ca31cd26f23129db9728fc38b8199d
Makauly Silva <makauly123@outlook.com>
2022-08-09 00:10:11 -0300

    exercicios com funções construtoras

--- curso-udemy/aula053-calculadora-function-construtora/assets/js/calc.js
+++ curso-udemy/aula053-calculadora-function-construtora/assets/js/calc.js
@@ -1,27 +1,65 @@
-function Criacalculadora() {
+//Function factory
+function Calculadora() {
     
-    const display = document.querySelector('.display');
+    this.display = document.querySelector('.display');
 
-    this.inicia = () => {
-        this.selecionaBotao
+    this.capturaCliques = () => { //Captura o ecento de cliques dos botoes
+        document.addEventListener('click', event => {
+            const el = event.target;
+            if(el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if(el.classList.contains('btn-clear')) this.clear();
+            if(el.classList.contains('btn-del')) this.del();
+            if(el.classList.contains('btn-eq')) this.realizaConta();
+            //Cada evento é despachado para um método
+
+            this.display.focus();
+        });
+    };
+    
+    //Método para colocar o número clicado no display
+    this.addNumDisplay = el => {
+        this.display.value += el.innerText;
     };
 
-    this.selecionaBotao = () => {
-        document.addEventListener('click', (e) => {
-
-            const elemento = e.target;
-            
-            if(elemento.classList.contains('btn-num')) {
-                this.botaoNum(elemento.innerText);
-            }
+    this.inicia = () => {  // Vai checar pelo capturaCliques e capturaEnter colocado nesta function
+        this.capturaCliques(); 
+        this.capturaEnter();
+    };
+    //Método que faz com que o enter realize as contas ao ser clicado
+    this.capturaEnter = () => {
+        document.addEventListener('keyup', e => {
+            if (e.keyCode !== 13) return;
+            this.realizaConta();
         })
-    };
+    }
+    
+    //Método para limpar o display
+    this.clear = () => this.display.value = ''; //Arrow function reduzida, funciona qnd a function tem somente uma linha
+    
+    //Método que apaga o ultimo elemento do display
+    this.del = () => this.display.value = this.display.value.slice(0, -1);  // Outra arrow function reduzida
+     
+    // Método que irá tentar fazer com que o js avalie o que esta no display como código js, fazendo assim os calculos (ele avaliara como conta)
+    this.realizaConta = () => {
+        try {
+            const conta = eval(this.display.value);
 
-    this.botaoNum = (valor) => {
-        display.value += valor;
-    };
+            if(!conta) {
+                alert('Atenção esta conta é inválida');
+                return; //Se for NaN 
+            }
+             
+            this.display.value = conta; //Se ele avaliar como conta
 
-   
+           
+        }catch(error) {
+
+            alert('Atenção esta conta é inválida');
+                return;
+        }
+    }
+
 }
 
-const calculadora = Criacalculadora();
+const calculadora = new Calculadora(); //Nova instancia 
+calculadora.inicia(); //Chamo o método inicia dentro da função
