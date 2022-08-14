f8c02f575877076f8e3a6d6a2b6d8c8e0ecfb6b6
Makauly Silva <makauly123@outlook.com>
2022-08-14 09:50:33 -0300

    exercicios

--- exercicios-git-hub/ex03-semaforo/semaforo03/semaforo03.js
+++ exercicios-git-hub/ex03-semaforo/semaforo03/semaforo03.js
@@ -1,28 +1,31 @@
 const img = document.getElementById('img');
 const buttons = document.getElementById('buttons');
-let indexOfCores = 0;
-intervalId = null;
+let proximaCor = 0;
+let controlaLuzes = null;
 
-const lights = (event) => {
-    paraSemaforo();
-    luzesAcessas[event.target.id]();
-}
+const iniciaSemaforo = (event) => {
+    paraAutomaticamente();
+    ligaLuzes[event.target.id]();
+};
 
-const luzesAutomaticas = () => indexOfCores = indexOfCores < 2 ? ++indexOfCores : 0;   
-
-const coresAutomaticas = () => {
+const automaticoBotao = () => {
     const cores = ['red', 'yellow', 'green'];
-    const ligaCores = cores[indexOfCores];
-    luzesAcessas[ligaCores]();
-    luzesAutomaticas();
-}
-const paraSemaforo = () => {
-    clearInterval(intervalId);
-}
-const luzesAcessas = {
+    const cor = cores[proximaCor];
+    ligaLuzes[cor]();
+    lacoLuzes(); 
+};
+
+const lacoLuzes = () => proximaCor = proximaCor < 2 ? ++proximaCor : 0;
+
+const paraAutomaticamente = () => {
+    clearInterval(controlaLuzes);
+}; 
+
+const ligaLuzes = {
     'red': () => img.src = './img/vermelho.png',
     'yellow': () => img.src = './img/amarelo.png',
     'green': () => img.src = './img/verde.png',
-    'automatic': () => intervalId = setInterval(coresAutomaticas, 1000),
-}
-buttons.addEventListener('click', lights);
+    'automatic': () => controlaLuzes = setInterval(automaticoBotao, 10000)
+};
+
+buttons.addEventListener('click', iniciaSemaforo);
