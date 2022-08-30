0c68ee5cd9aac85b9af2c1d308fd34b9aacca322
MakaulyDaSilva <makauly123@outlook.com>
2022-08-29 23:41:56 -0300

    atualizando exercicio

--- exercicios-git-hub/ex08-to-do-list/to-do-list-01/app.js
+++ exercicios-git-hub/ex08-to-do-list/to-do-list-01/app.js
@@ -3,14 +3,14 @@
 //2º
 //Crio o array de objetos do banco de dados com as tarefas (variavel global)
 let banco = [
-    {'tarefa': 'Estudar JS', 'status': ''},
-    {'tarefa': 'netflix' , 'status': 'checked'},
-    {'tarefa': 'teste' , 'status': 'checked'}
+    { 'tarefa': 'Estudar JS', 'status': '' },
+    { 'tarefa': 'netflix', 'status': 'checked' },
+    { 'tarefa': 'teste', 'status': 'checked' }
 ];
 
 //**1º
 //Function que recebe os parametros para manipular o dom
-const criarItem = (tarefa, status='') => {
+const criarItem = (tarefa, status = '') => {
     //crio o elemento
     const item = document.createElement('label');
     //adiciono a classe
@@ -25,13 +25,23 @@ const criarItem = (tarefa, status='') => {
     document.getElementById('todoList').appendChild(item);
 }
 
-//3°
+//3º
 //Function que pega as tarefas do banco e coloca nos argumentos da function criar item usando o forEach que percorre cada elemento do array
 const atualizarTela = () => {
     //Chamo a função
     limparTarefas();
     //coloco a tarefa e o status do banco para aparecer no dom
     banco.forEach(item => criarItem(item.tarefa, item.status));
+}
+
+//4º
+//Função que limpa as tarefas, ela será colocada no atualizar tarefas para ser executada primeiro
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList');
+    //Enquanto existir o primeiro filho remove o ultimo filho da div pai, assim ele sempre ira excluir o úlltimo elemento da div pai
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
 }
 
 atualizarTela();
