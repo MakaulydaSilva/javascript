638c077d4179d7d3743df21fb403ec76eced43de
MakaulyDaSilva <makauly123@outlook.com>
2022-09-02 23:30:52 -0300

    continuando exercicio de  condições

--- exercicios-git-hub/ex08-to-do-list/meu teste/script.js
+++ exercicios-git-hub/ex08-to-do-list/meu teste/script.js
@@ -1,23 +1,37 @@
+let select = document.getElementById('pergunta1');
+let texto = document.getElementById('resposta');
 
+const pergunta1 = () => {
+    let escolha = select.value;
 
-const criaResposta = (suaCasa) => {
-    const elemento = document.createElement('p');
-    elemento.classList.add('p_class');
-    elemento.innerHTML = `PARABÊNS!! Você pertence a ${suaCasa}`;
-    document.getElementById('resposta').appendChild(elemento);
-}
-
-const p1 = document.getElementById('p1');
-const p4 = document.getElementById('p4');
-
-const qualSuaCasa = evento => {
-    let evento = evento.key;
-    
-    if( evento ===  'Q') {
-        criaResposta('Grifinoria')
+    if (escolha === 'p1') {
+        texto.textContent = 'É preciso ter coragem para enfrentar os inimigos, e ainda mais para enfrentar os amigos.'
+    } else if (escolha === 'p2') {
+        texto.textContent = 'Coragem não signigicada não ter medo, mas sim ter força para enfrenta-los.'
+    } else if (escolha === 'p3') {
+        texto.textContent = 'Você foi sincero, isso de certa forma é coragem';
+    } else {
+        texto.textContent = '';
     }
 }
 
-document.getElementById('p1').addEventListener('keypress', qualSuaCasa);
-//document.getElementById('p4').addEventListener('keypress', qualSuaCasa);
+const textoSuaCasa = (suaCasa) => {
+    const elemento = document.createElement('p');
+    elemento.classList.add('p_class');
+    elemento.innerHTML = `PARABÊNS!! Você pertence a ${suaCasa}`;
+    document.getElementById('suaCasa').appendChild(elemento);
+}
 
+const suaCasa = () => {
+    let escolha = select.value;
+    if (escolha === 'p1') {
+        return textoSuaCasa('GRIFINÓRIA');
+    } else if (escolha === 'p2') {
+        return textoSuaCasa('CORVINAL');
+    } else if (escolha === 'p3') {
+        return textoSuaCasa('LUFA-LUFA');
+    }
+}
+
+select.addEventListener('change', pergunta1);
+document.addEventListener('change', suaCasa);
